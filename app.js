67a9945acb64beeb95f699d4a3ebd0258b7f5ff9
hisdot <103729397+hisdot@users.noreply.github.com>
2022-06-11 19:34:41 +0900

    return과 조건문 노마드코더 과제입니다.

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-console.log(daysOfWeek[4]); //array
-daysOfWeek.push("new");
-console.log(daysOfWeek);
+const age = parseInt(prompt("how old are you?"));
 
-const Dubai = ["potato", "tamato", "pizza"];
-Dubai.push("kimbab"); //인덱스는 0부터 시작
-console.log(Dubai[2]);
-console.log(Dubai.length);
+if (isNaN(age) || age < 0) {
+    console.log("please write a real positive number");
+} else if (age < 18) {
+    console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if (age > 80) {
+    console.log("You can do whatever you want.");
+}
 
-const player = {
-    name: 'hisdot',
-    points: 100,
-    fat: true,
-};
-console.log(player);
-console.log(player.name); //this is 객체
+// true || true === true
+// false || true === true
+// true || false === true
+// false || false === false
 
-player.strength = 'strong';
-console.log(player.strength);
-
-player.points = player.points + 230;
-console.log(player.points);
+// true && true === true
+// false && true === false
+// true && false === false
+// false && false === false
